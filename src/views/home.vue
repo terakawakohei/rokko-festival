6f75a0272c0cc5798760e7a42abb8b0b61dc7a3d
kobekucc <kobe_u_manken@yahoo.co.jp>
2020-08-23 21:23:26 +0900

    Home.vue→home.vue

---
+++ src/views/home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-carousel
+      cycle
+      height="400"
+      hide-delimiter-background
+      show-arrows-on-hover
+    >
+      <v-carousel-item v-for="(slide, i) in slides" :key="i">
+        <v-sheet :color="colors[i]" height="100%">
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="display-3">{{ slide }} Slide</div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+
+    <Description />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Description from '@/views/description.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Description,
+  },
+  data() {
+    return {
+      colors: [
+        'indigo',
+        'warning',
+        'pink darken-2',
+        'red lighten-1',
+        'deep-purple accent-4',
+      ],
+      slides: ['First', 'Second', 'Third', 'Fourth', 'Fifth'],
+    };
+  },
+};
+</script>
+<style></style>
