93445767853f64aa09e84ab1fb36c08176cfcc58
kobekucc <kobe_u_manken@yahoo.co.jp>
2020-08-24 16:09:24 +0900

    埋め込み試す

--- src/views/offset.vue
+++ src/views/offset.vue
@@ -3,6 +3,15 @@
     <v-row justify="center">
       <h1 class="display-1 font-weight-black">オフセット本</h1>
     </v-row>
+    <a
+      href="https://kucc-rokkofestival.herokuapp.com/bibi/?book=Miserables.epub"
+      data-bibi="embed"
+      >レミゼラブル</a
+    >
+    <div
+      is="”script”"
+      src="https://kucc-rokkofestival.herokuapp.com/bibi/and/jo.js"
+    ></div>
   </div>
 </template>
 
